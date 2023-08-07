f26d04d0a290d49c577d70363b06fc0fa35bc608
wanli.yuan <wanli.yuan@tongdun.net>
2023-08-07 14:22:50 +0800

    启动类增加日志打印

--- src/main/java/com/ywl/springboot/SpringbootApplication.java
+++ src/main/java/com/ywl/springboot/SpringbootApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * @author Administrator
+ */
 @MapperScan(value = "com.ywl.springboot.mapper")
 @SpringBootApplication
 @EnableCaching
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
+		System.out.println("启动成功");
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 

ffa4221a8aa2b11c925423f4d01fe863b854df53
袁万里 <1213216150@example.com>
2025-08-05 13:33:14 +0800

    [TEST] 主类接口扫描

--- src/main/java/com/ywl/Application.java
+++ src/main/java/com/ywl/Application.java
@@ -2,11 +2,13 @@ package com.ywl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Create by ywl--- 2021-07-21-16:11
  **/
 @SpringBootApplication
+@ComponentScan("com.ywl.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
